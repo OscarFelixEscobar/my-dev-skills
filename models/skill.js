e0d26a5b17c0ed3c62d172de6ebb2fb69193a134
OscarFelixEscobar <oscarfelixescobar@gmail.com>
2022-03-18 15:02:52 -0700

    adds router changed

---
+++ models/skill.js
@@ -0,0 +1,14 @@
+const skills = [
+    {id: 132433, skill: "Javascript"},
+    {id: 823712, skill: "CSS"},
+    {id: 734823, skill: "HTML"}
+
+];
+
+module.exports = {
+    getAll
+};
+
+function getAll() {
+    return skills;
+}
